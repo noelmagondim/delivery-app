80f171d4df79b1ae8dd9621557099ff2a1576fc8
André Melo <adr.smelo@gmail.com>
2022-09-21 20:12:57 -0300

    feat: implements route for the endpoint GET /login/validate

--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -5,5 +5,6 @@ const userRouter = Router();
 
 userRouter.post('/login', userController.login);
 userRouter.post('/register', userController.create);
+userRouter.get('/login/validate', userController.loginValidate);
 
 module.exports = userRouter;
