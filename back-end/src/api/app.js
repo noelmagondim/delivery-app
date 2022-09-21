ca9cec2bd5b969ea5197270a974e1682819b89cf
Matheus Pigatto <1304matheus@gmail.com>
2022-09-20 23:01:38 -0300

    feat: added custom error handler and error

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+require('express-async-errors');
+const CustomError = require('../errors/CustomError');
+const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/coffee', (req, res) => {
+  throw new CustomError(401, 'You need to login to access this resource');
+
+  res.status(418).end();
+});
+
+app.use(errorHandler)
 
 module.exports = app;
