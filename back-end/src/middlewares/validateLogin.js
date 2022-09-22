7a1064972744656f31333c88637558e1c054b08f
Matheus Pigatto <1304matheus@gmail.com>
2022-09-22 17:27:13 -0300

    feat: added user validations

---
+++ back-end/src/middlewares/validateLogin.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+const CustomError = require('../errors/CustomError');
+
+const validateLogin = (req, _res, next) => {
+  const sale = req.body;
+
+  const schema = Joi.object({
+    email: Joi.email().required(),
+    password: Joi.string().required().min(6),
+  }).required();
+
+  const { error } = schema.validate(sale);
+
+  if (error) {
+    throw new CustomError(400, error.message);
+  }
+
+  next();
+}; 
+
+module.exports = validateLogin;
--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const userController = require('../controllers/user.controller');
+const validateUser = require('../middlewares/validateUser');
+const validateLogin = require('../middlewares/validateLogin');
 const auth = require('../middlewares/auth');
 
 
 const userRouter = Router();
 
-userRouter.post('/login', userController.login);
-userRouter.post('/register', userController.create);
+userRouter.post('/login', validateLogin, userController.login);
+userRouter.post('/register', validateUser, userController.create);
 userRouter.get('/', auth, userController.getAll);
 userRouter.get('/:id', auth, userController.findUserById);
 userRouter.get('/login/validate', userController.loginValidate);
