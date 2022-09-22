9edeea8abeff8c132ce5bc2a0e36947e5064e2d4
André Melo <adr.smelo@gmail.com>
2022-09-22 14:48:26 -0300

    refactor: ESLint fix

--- back-end/src/routes/sale.routes.js
+++ back-end/src/routes/sale.routes.js
@@ -3,12 +3,12 @@ const validateSale = require('../middlewares/validateSale');
 const validateSaleStatus = require('../middlewares/validateSaleStatus');
 const saleController = require('../controllers/sale.controller');
 
- const saleRouter = Router();
+const saleRouter = Router();
 
- saleRouter.post('/', validateSale, saleController.create);
- saleRouter.patch('/status/:saleId', validateSaleStatus, saleController.updateStatus);
- saleRouter.get('/:saleId', saleController.findById);
- saleRouter.get('/user/:userId', saleController.findByUser);
- saleRouter.get('/seller/:sellerId', saleController.findBySeller);
+saleRouter.post('/', validateSale, saleController.create);
+saleRouter.patch('/status/:saleId', validateSaleStatus, saleController.updateStatus);
+saleRouter.get('/:saleId', saleController.findById);
+saleRouter.get('/user/:userId', saleController.findByUser);
+saleRouter.get('/seller/:sellerId', saleController.findBySeller);
 
- module.exports = saleRouter;
+module.exports = saleRouter;
