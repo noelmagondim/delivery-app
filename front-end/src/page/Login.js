509f213036d22d52525e0ec43b21b381eabb8e9e
noelmagondim <noelmagondim@gmail.com>
2022-09-22 09:39:58 -0300

    Created Login page

---
+++ front-end/src/page/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LoginForm from '../components/LoginForm';
+
+export default function Login() {
+  return (
+    <div>
+      <Header />
+      <LoginForm />
+      <Footer />
+    </div>
+  );
+}
