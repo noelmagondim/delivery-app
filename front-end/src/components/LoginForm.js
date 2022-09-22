c0b9fe9a840cd774ca9923323fb4cc91f39804ca
noelmagondim <noelmagondim@gmail.com>
2022-09-22 10:21:20 -0300

    implementing login form

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LoginForm() {
+  const initialValue = {
+    email: '',
+    password: '',
+  };
+
+  const [values, setValues] = useState(initialValue);
+
+  const handleChange = (event) => {
+    values[event.target.name] = event.target.value;
+    setValues(values);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(values);
+  };
+
   return (
     <div>
-      <h1>Login</h1>
-      <label htmlFor="input-email">
-        Login
-        <input
-          data-testid="common_login__input-email"
-          type="email"
-          name="email"
-          id="input-email"
-          placeholder="email@trybeer.com.br"
-          required
-        />
-      </label>
-      <label htmlFor="input-password">
-        Senha:
-        <input
-          data-testid="common_login__input-password"
-          type="password"
-          name="password"
-          id="input-password"
-          required
-        />
-      </label>
-      <button
-        data-testid="common_login__button-login"
-        type="submit"
-      >
-        LOGIN
-      </button>
-      <button
-        data-testid="common_login__button-register"
-        type="submit"
-      >
-        Ainda não tenho conta
-      </button>
+      <form onSubmit={ handleFormSubmit }>
+        <label htmlFor="input-email">
+          Login
+          <input
+            data-testid="common_login__input-email"
+            type="email"
+            name="email"
+            id="input-email"
+            placeholder="email@trybeer.com.br"
+            onChange={ handleChange }
+            required
+          />
+        </label>
+        <label htmlFor="input-password">
+          Senha:
+          <input
+            data-testid="common_login__input-password"
+            type="password"
+            name="password"
+            id="input-password"
+            onChange={ handleChange }
+            required
+          />
+        </label>
+        <button
+          data-testid="common_login__button-login"
+          type="submit"
+        >
+          LOGIN
+        </button>
+        <button
+          data-testid="common_login__button-register"
+          type="submit"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
     </div>
   );
 }
