e5b827f725aa4c2642bf2a47ae0b37335a43829d
noelmagondim <noelmagondim@gmail.com>
2022-09-26 18:04:08 -0300

    Created Component CardPedido

---
+++ front-end/src/CardsPedidos/Components/CardPedido.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function CardPedido() {
+  const navigate = useNavigate();
+
+  return data.map((element, index) => (
+    <button
+      key={ index }
+      type="button"
+      onClick={ navigate('/customer/orders/:orderId') }
+    >
+      <p data-testid="customer_orders__element-order-id-<id>">
+        {element.oderId}
+      </p>
+
+      <p data-testid="customer_orders__element-delivery-status-<id>">
+        {element.status}
+      </p>
+
+      <p data-testid="customer_orders__element-order-date-<id>">
+        {element.sale_data}
+      </p>
+
+      <p data-testid="customer_orders__element-card-price-<id>">
+        {element.total_price}
+      </p>
+    </button>
+  ));
+}
