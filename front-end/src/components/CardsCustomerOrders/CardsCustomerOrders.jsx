55f212745d8c7e0caa43f0537c7e72adf2099c4e
noelmagondim <noelmagondim@gmail.com>
2022-09-26 20:43:35 -0300

    Feat: Implemented page CustomerOrders

--- front-end/src/components/CardsCustomerOrders/CardsCustomerOrders.jsx
+++ front-end/src/components/CardsCustomerOrders/CardsCustomerOrders.jsx
@@ -1,60 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import { requestCustomerOrders, setToken } from '../../services/requests';
 
-export default function CardsCustomerOrders() {
-  const [orders, setOrders] = useState([]);
-  const [message, setMessage] = useState('');
+export default function CardsCustomerOrders({ order }) {
   const navigate = useNavigate();
-
-  const getCustomerOrders = async () => {
-    setToken(localStorage.getItem('token'));
-
-    const orderId = localStorage.getItem('id');
-    const data = await requestCustomerOrders(orderId);
-    console.log(data);
-
-    if (data.status >= Number('400')) {
-      setMessage(data.data.message);
-      return;
-    }
-
-    setOrders(data);
-  };
-
-  useEffect(() => {
-    getCustomerOrders();
-  }, []);
-
+  console.log(order);
   return (
     <div>
-      {
-        (orders.length === 0)
-          ? <p>{ message }</p>
-          : orders.map((order, index) => (
-            <button
-              key={ index }
-              type="button"
-              onClick={ navigate`/customer/orders/:${order.id}` }
-            >
-              <p data-testid="customer_orders__element-order-id-<id>">
-                {order.orderId}
-              </p>
+      <button
+        type="button"
+        onClick={ navigate`/customer/orders/:${order.id}` }
+      >
+        <p data-testid="customer_orders__element-order-id-<id>">
+          {order.id}
+        </p>
 
-              <p data-testid="customer_orders__element-delivery-status-<id>">
-                {order.status}
-              </p>
+        <p data-testid="customer_orders__element-delivery-status-<id>">
+          {order.status}
+        </p>
 
-              <p data-testid="customer_orders__element-order-date-<id>">
-                {order.saleDate}
-              </p>
+        <p data-testid="customer_orders__element-order-date-<id>">
+          {order.saleDate}
+        </p>
 
-              <p data-testid="customer_orders__element-card-price-<id>">
-                {order.totalPrice}
-              </p>
-            </button>
-          ))
-      }
+        <p data-testid="customer_orders__element-card-price-<id>">
+          {order.totalPrice}
+        </p>
+      </button>
     </div>
   );
 }
+
+CardsCustomerOrders.propTypes = {
+  order: PropTypes.objectOf(PropTypes.string).isRequired,
+};
--- front-end/src/page/CustomerOrders.jsx
+++ front-end/src/page/CustomerOrders.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import CardsCustomerOrders from '../components/CardsCustomerOrders/CardsCustomerOrders';
+import { requestCustomerOrders, setToken } from '../services/requests';
 
 export default function CostumerOrders() {
+  const [orders, setOrders] = useState([]);
+
+  const getCustomerOrders = async () => {
+    setToken(localStorage.getItem('token'));
+
+    const orderId = localStorage.getItem('id');
+    const { data } = await requestCustomerOrders(orderId);
+    console.log(data);
+    setOrders(data);
+  };
+
+  useEffect(() => {
+    getCustomerOrders();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <h1>Pedidos</h1>
-      <CardsCustomerOrders />
+      <div>
+        {
+          orders && orders.map((order, index) => (
+            <CardsCustomerOrders order={ order } key={ index } />
+          ))
+        }
+      </div>
       <Footer />
     </div>
   );
